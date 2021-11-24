c739367ffc8b4a1e513b0cea94e09c4fcb016a3b
Tadhg <tadhgsmboyle@gmail.com>
2021-11-24 09:57:36 -0800

    upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const PORT = process.env.PORT || 8007;
 const app = express();
+const database = require("./database.json");
+const fs = require("fs").promises;
 
 // Don't worry about these 4 lines below
 app.set("view engine", "ejs");
@@ -11,8 +13,35 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("homepage");
 });
+
 app.get("/people/:id", (req, res) => {
-  res.render("people");
+  res.render("people", {
+    ...getUserFromDatabase(req.params.id),
+  });
+});
+
+app.get("/create", (req, res) => {
+  res.render("createcard");
+});
+
+app.post("/create", (req, res) => {
+  const newUser = {
+    id: database.users.length + 1,
+    fullName: req.body.fullName,
+    aboutMe: req.body.aboutMe,
+    knownTechnologies: req.body.knownTechnologies ?? null,
+    githubUrl: req.body.githubUrl,
+    twitterUrl: req.body.twitterUrl,
+    favoriteBooks: req.body.favoriteBooks?.split(","),
+    favoriteArtists: req.body.favoriteArtists?.split(","),
+  }
+
+  database.users.push(newUser);
+
+  fs.writeFile(
+      "./database.json",
+      JSON.stringify(database, null, 2),
+  ).then(_ => res.redirect(`/people/${newUser.id}`));
 });
 
 app.get("/:id/photos", (req, res) => {
@@ -22,3 +51,12 @@ app.get("/:id/photos", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server now is running at http://localhost:${PORT} 🚀`);
 });
+
+const getUserFromDatabase = id => {
+  for (const user of database.users) {
+    if (user.id == id) {
+      return user;
+    }
+  }
+  return null;
+}
